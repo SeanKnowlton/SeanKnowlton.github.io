b599c092a733f8d5c4322a7e000999e224219cdf
Knowlton <sean@tulane.edu>
2019-06-04 15:34:02 -0500

    made a thing in which it prints only my name

---
+++ hilt_class.py
@@ -0,0 +1,5 @@
+#making a list of peoples' names in the class
+hilt_class =  ["Haley", "Sean", "Michelle", "AJ"]
+for name in hilt_class:
+        if name == "Sean":
+            print("My name is " + name)
