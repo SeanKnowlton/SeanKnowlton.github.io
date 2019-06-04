5ca43257c924c4c24b6c581b7e6c6a67b98bd6ed
Knowlton <sean@tulane.edu>
2019-06-04 16:00:24 -0500

    apple baby

---
+++ python/apple_branch.py
@@ -0,0 +1,11 @@
+fruits = ["banana", "jellybean", "spaceship"] 
+if "apple" not in fruits[0]:
+	print("Yuck")
+
+age = 5
+if age > 0 and age <= 2:
+	print("baby")
+elif age > 2 and age < 18:
+	print("child")
+else:
+	print("adult")
