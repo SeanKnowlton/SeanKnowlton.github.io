6e2b2cb40521f1616696fec4dcec76ae30f75740
Knowlton <sean@tulane.edu>
2019-06-04 14:03:09 -0500

    some python dealings

---
+++ address.py
@@ -0,0 +1,13 @@
+name = {
+            "first_name": "Sean",
+            "last_name": "Knowlton",
+            }
+street = {
+            "street_number": "2331",
+            "street_name": "Dauphine",
+            "street_type": "Street",
+            }
+city = {
+        "city": "New Orleans",
+            "zip": "70117"
+            }
---
+++ address2.py
@@ -0,0 +1,12 @@
+address = {
+            "first_name": "Sean",
+            "last_name": "Knowlton",
+            "street_number": "2331",
+            "street_name": "Dauphine",
+            "street_type": "Street",
+            "city": "New Orleans",
+            "zip": "70117"
+            }
+print(address["first_name"] + " " + address["last_name"])
+print(address["street_number"] + " " + address["street_name"] + " " + address["street_type"])
+print(address["city"] + " " + address["zip"])
