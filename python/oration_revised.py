595d87288621ffc2c71307f6b4d8fd5184b5788d
Knowlton <sean@tulane.edu>
2019-06-05 10:24:34 -0500

    revised this entirely to take a dicitonary approach

---
+++ python/oration_revised.py
@@ -0,0 +1,9 @@
+#define a collection
+name_origin = {'Miguelito': 'Mexico',
+              'Jose': 'Honduras',
+              'Ricarda': 'Brasil',
+              'Juliana': 'Cuba'
+              }
+
+for name, origin in name_origin.items():
+        print("Hola, mi nombre es " + name + ". Yo soy de " + origin + ".")
