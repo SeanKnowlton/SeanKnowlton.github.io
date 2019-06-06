f6ea53edf9ec1d9064966764113abdfa0f1b9829
Knowlton <sean@tulane.edu>
2019-06-06 10:32:54 -0500

    did some additional exercises

--- python/shopping.py
+++ python/shopping.py
@@ -3,6 +3,19 @@ import csv
 groceries = []
 with open('csvs/shopping.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in reader:
-        groceries.append(row)
-print(groceries)
+    #for row in reader:
+        #groceries.append(row)
+#print(groceries[2]['name'])
+
+#for item in groceries:
+     #print(item['name'])
+
+tony = {'name': 'tony', 'nutritious': 'False', 'id': '5'}
+groceries.append(tony)
+
+with open ('csvs/updated_groceries.csv', 'w', newline='') as fout:
+    column_names = ['id', 'name', 'nutritious']
+    dictwriter = csv.DictWriter(fout, fieldnames=column_names)
+    dictwriter.writeheader()
+    for row in groceries:
+        dictwriter.writerow(row)
